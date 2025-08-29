6d2c6339b7ec0354e41f0944fd4c4e2ea8b09617
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-08-29 06:57:33 +0530

    k8-eksctl

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+  backend"s3"{
+    bucket="dev-daw9"
+     key= "expense-vpc-p/terraform.tfstate" 
+    region="us-east-1"
+    dynamodb_table="daws83s"
+ 
+}
+}
+provider "aws" {
+ region="us-east-1"
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-05d876ace7467e5b6"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-063a992d2b4ceb970"
+}
