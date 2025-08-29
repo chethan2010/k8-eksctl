f9834976c9a9a5c3b1a16d899036f61b6fb2d704
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-08-29 17:39:30 +0530

    k8-eksctl

--- provider.tf
+++ provider.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+     bucket="dev-daw9"
+     key= "k8s-dev/terraform.tfstate"
+    region="us-east-1"
+    dynamodb_table="daws83s"
+  }
+}
+
+#provide authentication here
 provider "aws" {
   region = "us-east-1"
 }
