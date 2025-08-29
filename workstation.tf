9cf3020af487b25b50d0aa865999634f1048cd8b
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-08-29 07:11:10 +0530

    k8-eksctl

--- workstation.tf
+++ workstation.tf
@@ -8,7 +8,7 @@ module "workstation" {
   # convert StringList to list and get first element
   subnet_id = var.public_subnet_id
   ami = data.aws_ami.ami_info.id
-  user_data = file("workstation.sh")
+  user_data = file("worksstation.sh")
   tags = {
         Name = "workstation"
     }
